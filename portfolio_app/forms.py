72018d72602ea74706079b26b8006e3e48ec84fe
Chrysanthus Chiagwah <chrysanthusobinna@gmail.com>
2025-01-14 13:08:26 +0000

    Update signup form to collect username and update settings

--- portfolio_app/forms.py
+++ portfolio_app/forms.py
@@ -4,12 +4,12 @@ from django import forms
 class CustomSignupForm(SignupForm):
     first_name = forms.CharField(max_length=30, label="First Name", required=True)
     last_name = forms.CharField(max_length=30, label="Last Name", required=True)
+    username = forms.CharField(max_length=30, label="Username", required=True)
 
     def save(self, request):
         user = super(CustomSignupForm, self).save(request)
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
-        # Set username to email to avoid username-related issues
-        user.username = user.email
+        user.username = self.cleaned_data['username']
         user.save()
         return user
