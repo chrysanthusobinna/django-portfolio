6cd484e2b88cfe90b96754c49357e04ad56a5dae
Chrysanthus Chiagwah <chrysanthusobinna@gmail.com>
2025-01-14 20:06:31 +0000

    refactor(models): Added start and end dates for About and Employment models, updated views and templates to reflect changes

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -15,6 +15,8 @@ class Employment(models.Model):
     employer_name = models.CharField(max_length=255)
     job_title = models.CharField(max_length=255)
     description_of_duties = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.job_title} at {self.employer_name}"
@@ -25,7 +27,7 @@ class Education(models.Model):
     qualification = models.CharField(max_length=100)
     institution_name = models.CharField(max_length=200)
     start_date = models.DateField()
-    end_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.qualification} from {self.institution_name}"
