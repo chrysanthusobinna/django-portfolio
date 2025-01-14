4fd9675ba0db8f5057c8dc797ec8132d2cc81d75
Chrysanthus Chiagwah <chrysanthusobinna@gmail.com>
2025-01-14 20:27:31 +0000

    Fix user profile view to handle user not found and update base template with new UI elements

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from .models import  About, Employment, Certification, Education, Portfolio, Contact
 from django.contrib.auth.models import User
+from django.shortcuts import redirect
+from django.contrib import messages
+
 
 def home(request):
     return render(request, 'home-page.html')
 
 def user_profile(request, username):
-    target_user = get_object_or_404(User, username=username)
-    
+    try:
+        target_user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        messages.error(request, "The user you are looking for does not exist.")
+        # Redirect to the home page
+        return redirect('home')
+
     # Get the related data for the user, or set to None if not found
     about = About.objects.filter(user=target_user).first() or None
     employment = Employment.objects.filter(user=target_user) or None
