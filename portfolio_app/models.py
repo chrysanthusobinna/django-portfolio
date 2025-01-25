8a2ccfb22c8d2438f0893b94944c4535978a23fe
Chrysanthus Chiagwah <chrysanthusobinna@gmail.com>
2025-01-25 10:49:25 +0000

    refactor model to comply with PEP 8 standards

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -2,12 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+
 class Profilephoto(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_photo = CloudinaryField('image', null=True, blank=True)
 
     def __str__(self):
         return f"Profile photo for {self.user.username}"
+
 
 # About model
 class About(models.Model):
@@ -16,6 +18,7 @@ class About(models.Model):
 
     def __str__(self):
         return f"About {self.user.username}"
+
 
 # Employment model
 class Employment(models.Model):
@@ -29,9 +32,11 @@ class Employment(models.Model):
     def __str__(self):
         return f"{self.job_title} at {self.employer_name}"
 
+
 # Education Section Model
 class Education(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='educations')
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='educations')
     qualification = models.CharField(max_length=100)
     institution_name = models.CharField(max_length=200)
     start_date = models.DateField()
@@ -40,9 +45,11 @@ class Education(models.Model):
     def __str__(self):
         return f"{self.qualification} from {self.institution_name}"
 
+
 # Certification Section Model
 class Certification(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='certifications')
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='certifications')
     name = models.CharField(max_length=200)
     issuer = models.CharField(max_length=200)
     date_issued = models.DateField()
@@ -50,9 +57,11 @@ class Certification(models.Model):
     def __str__(self):
         return f"{self.name} by {self.issuer}"
 
+
 # Portfolio Section Model
 class Portfolio(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='portfolios')
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='portfolios')
     title = models.CharField(max_length=200)
     description = models.TextField()
     link = models.URLField(null=True, blank=True)
@@ -61,9 +70,11 @@ class Portfolio(models.Model):
     def __str__(self):
         return self.title
 
+
 # Contact Section Model
 class Contact(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='contacts')
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='contacts')
     phone_number = models.CharField(max_length=20)
     email_address = models.EmailField()
     linkedin = models.URLField(null=True, blank=True)
