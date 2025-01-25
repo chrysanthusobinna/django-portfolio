b1cd05dd4a9e4990d7a7ffd50205dddfa3cb4b5c
Chrysanthus Chiagwah <chrysanthusobinna@gmail.com>
2025-01-25 11:21:16 +0000

    refactor admin.py to comply with PEP 8 standards

--- portfolio_app/admin.py
+++ portfolio_app/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Education, Certification, Portfolio, Contact, About, Employment
-
-admin.site.register(About)
-admin.site.register(Employment)
-admin.site.register(Education)
-admin.site.register(Certification)
-admin.site.register(Portfolio)
-admin.site.register(Contact)
+from .models import (
+    Education,
+    Certification,
+    Portfolio,
+    Contact,
+    About,
+    Employment
+)
