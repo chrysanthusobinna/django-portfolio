fdb08300f777417c9a0e8f5c2825485ca61f6c5b
Chrysanthus Chiagwah <chrysanthusobinna@gmail.com>
2025-01-25 11:05:41 +0000

    refactor functions validate_image_file and send_contact_email in utils.py to align with PEP 8 standards

--- portfolio_app/utils.py
+++ portfolio_app/utils.py
@@ -2,14 +2,19 @@ from django.contrib import messages
 from django.core.mail import send_mail
 from django.conf import settings
 
-def validate_image_file(request, file_field_name, label_name, allowed_extensions=('jpg', 'jpeg', 'png', 'gif')):
-    """
-    Validates if the uploaded file is an image with an allowed extension.
-    """
+
+def validate_image_file(request, file_field_name, label_name):
+    allowed_extensions = ('jpg', 'jpeg', 'png', 'gif')
+    error_msg = (
+        "Invalid file type for {label_name}. Only "
+        "{extensions} files are allowed."
+    ).format(label_name=label_name, extensions=', '.join(allowed_extensions))
+
+    # Validates if the uploaded file is an image with an allowed extension.
     if file_field_name in request.FILES:
         file = request.FILES[file_field_name]
         if not file.name.lower().endswith(allowed_extensions):
-            error_message = f"Invalid file type for {label_name}. Only {', '.join(allowed_extensions)} files are allowed."
+            error_message = error_msg
             return False, error_message
     return True, None
 
@@ -23,8 +28,7 @@ def send_contact_email(subject, message, recipient_list):
             recipient_list,
             fail_silently=False,
         )
-        return True, None  
+        return True, None
     except Exception as e:
         error_message = f"Error sending email: {e}"
-        print(f"Error sending email: {e}")
-        return False, error_message   
+        return False, error_message
