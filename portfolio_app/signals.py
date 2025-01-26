ca21da93ffbe3456c7c37c6217c0f806a436cd94
Chrysanthus Chiagwah <chrysanthusobinna@gmail.com>
2025-01-26 20:56:19 +0000

    Add custom message for user registration and fix NameError by importing user_signed_up signal

--- portfolio_app/signals.py
+++ portfolio_app/signals.py
@@ -1,8 +1,16 @@
-from allauth.account.signals import user_logged_in
+from allauth.account.signals import user_logged_in, user_signed_up
 from django.contrib import messages
 from django.dispatch import receiver
+
 
 @receiver(user_logged_in)
 def custom_user_logged_in_message(request, user, **kwargs):
     first_name = user.first_name if user.first_name else user.email
     messages.success(request, f"Welcome , {first_name}!")
+
+
+@receiver(user_signed_up)
+def custom_user_signed_up_message(request, user, **kwargs):
+    success_msg = "Registration successful!"
+    first_name = user.first_name if user.first_name else user.email
+    messages.success(request, f"{success_msg}!")
