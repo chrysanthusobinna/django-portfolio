17233715cadf708b1e83dedef911ac4425bdfbda
chrysanthus obinna <chrysanthusobinna@gmail.com>
2026-02-19 19:01:13 +0000

    Fix template preview footer links to point to main domain
    - Add MAIN_DOMAIN_URL context processor for proper domain resolution
    - Update all template preview footers to use absolute main domain URLs
    - Prevent footer links from staying on subdomains

--- portfolio_app/context_processors.py
+++ portfolio_app/context_processors.py
@@ -2,9 +2,16 @@ from django.conf import settings
 
 
 def site_info(request):
+    # Determine the main domain URL
+    if 'localhost' in request.get_host() or '127.0.0.1' in request.get_host():
+        main_domain_url = 'http://localhost:8000'
+    else:
+        main_domain_url = f'https://{settings.BASE_DOMAIN}'
+    
     return {
         'SITE_NAME': settings.SITE_NAME,
         'SITE_CONTACT_EMAIL_ADDRESS': settings.SITE_CONTACT_EMAIL_ADDRESS,
         'SITE_CONTACT_PHONE_NUMBER': settings.SITE_CONTACT_PHONE_NUMBER,
         'SITE_CONTACT_LINKEDIN_URL': settings.SITE_CONTACT_LINKEDIN_URL,
+        'MAIN_DOMAIN_URL': main_domain_url,
     }
