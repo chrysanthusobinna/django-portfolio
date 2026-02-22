939bfb73abfc45c7cddbe73a55831922c17044ac
chrysanthus obinna <chrysanthusobinna@gmail.com>
2026-02-22 00:20:04 +0000

    Update login redirect to home page

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -86,8 +86,9 @@ INSTALLED_APPS = [
 ]
 
 SITE_ID = 1
-LOGIN_REDIRECT_URL = '/get-started/'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+ACCOUNT_SIGNUP_REDIRECT_URL = '/get-started/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
